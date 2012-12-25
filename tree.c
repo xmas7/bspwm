xmas7/bspwm

commit 6ad8160fadb24e422ebb75c7ea855ff2b4f2e402

Correctly honour 'active_border_color' setting
Ensure the explicitly non-focused monitor with a focused window has its
borders down correctly when 'active_border_color' is used.

Previously, the checks handling this did not set the correct monitor to use.

tree.c
@@ -429,14 +429,17 @@ void focus_node(monitor_t *m, desktop_t *d, node_t *n, bool is_mapped)
     n->client->urgent = false;
 
     if (is_mapped) {
-        if (mon != m || d->focus != n) {
-            window_draw_border(d->focus, m != mon, m == mon);
-            window_draw_border(n, true, true);
-        }
-        if (focus_follows_mouse)
+		if (mon != m || d->focus != n) {
+			/* Honour active_border_color setting. */
+			window_draw_border(mon->desk->focus, true, false);
+			window_draw_border(d->focus, m != mon, m == mon);
+			window_draw_border(n, true, true);
+		}
+
+		if (focus_follows_mouse)
             get_pointer_position(&pointer_position);
-        xcb_set_input_focus(dpy, XCB_INPUT_FOCUS_POINTER_ROOT, n->client->window, XCB_CURRENT_TIME);
-    }
+		xcb_set_input_focus(dpy, XCB_INPUT_FOCUS_POINTER_ROOT, n->client->window, XCB_CURRENT_TIME);
+	}
 
     if (!is_tiled(n->client)) {
         if (!adaptative_raise || !might_cover(d, n))

commit 377baf529650e47a6bd2e794c071f564e7fa738b

Rewrite border drawing in `focus_node`
6ad8160fadb24e422ebb75c7ea855ff2b4f2e402 introduced a blinking border
bug under the `mon == m` circumstances, the new code tries to keep it
simple while being exhaustive.

tree.c
@@ -429,10 +429,17 @@ void focus_node(monitor_t *m, desktop_t *d, node_t *n, bool is_mapped)
     n->client->urgent = false;
 
     if (is_mapped) {
-        if (mon != m || d->focus != n) {
-            /* honour active_border_color setting */
-            window_draw_border(mon->desk->focus, true, false);
-            window_draw_border(d->focus, m != mon, m == mon);
+        if (mon != m) {
+            for (desktop_t *cd = mon->desk_head; cd != NULL; cd = cd->next)
+                window_draw_border(cd->focus, true, false);
+            for (desktop_t *cd = m->desk_head; cd != NULL; cd = cd->next)
+                if (cd != d)
+                    window_draw_border(cd->focus, true, true);
+            if (d->focus == n)
+                window_draw_border(n, true, true);
+        }
+        if (d->focus != n) {
+            window_draw_border(d->focus, false, true);
             window_draw_border(n, true, true);
         }
         if (focus_follows_mouse)