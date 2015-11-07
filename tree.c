xmas7/bspwm

commit f28f33448fb14e01c014fd2c66a6005e1f885900

Make a clear distinction between states and flags

tree.c
@@ -69,42 +69,39 @@ void apply_layout(monitor_t *m, desktop_t *d, node_t *n, xcb_rectangle_t rect, x
 	if (is_leaf(n)) {
 
 		unsigned int bw;
-		if ((borderless_monocle && !n->client->floating &&
-		     !n->client->pseudo_tiled &&
-		     d->layout == LAYOUT_MONOCLE) ||
-		    n->client->fullscreen)
+		if ((borderless_monocle && n->client->state == STATE_TILED && d->layout == LAYOUT_MONOCLE)
+		    || n->client->state == STATE_FULLSCREEN) {
 			bw = 0;
-		else
+		} else {
 			bw = n->client->border_width;
+		}
 
 		xcb_rectangle_t r;
-		if (!n->client->fullscreen) {
-			if (!n->client->floating) {
-				int wg = (gapless_monocle && d->layout == LAYOUT_MONOCLE ? 0 : d->window_gap);
-				if (n->client->pseudo_tiled) {
-				/* pseudo-tiled clients */
-					r = n->client->floating_rectangle;
-					if (center_pseudo_tiled) {
-						r.x = rect.x - bw + (rect.width - wg - r.width) / 2;
-						r.y = rect.y - bw + (rect.height - wg - r.height) / 2;
-					} else {
-						r.x = rect.x;
-						r.y = rect.y;
-					}
-				} else {
-					/* tiled clients */
-					r = rect;
-					int bleed = wg + 2 * bw;
-					r.width = (bleed < r.width ? r.width - bleed : 1);
-					r.height = (bleed < r.height ? r.height - bleed : 1);
-				}
-				n->client->tiled_rectangle = r;
+		client_state_t s = n->client->state;
+		if (s == STATE_TILED || s == STATE_PSEUDO_TILED) {
+			int wg = (gapless_monocle && d->layout == LAYOUT_MONOCLE ? 0 : d->window_gap);
+			/* tiled clients */
+			if (s == STATE_TILED) {
+				r = rect;
+				int bleed = wg + 2 * bw;
+				r.width = (bleed < r.width ? r.width - bleed : 1);
+				r.height = (bleed < r.height ? r.height - bleed : 1);
+			/* pseudo-tiled clients */
 			} else {
-				/* floating clients */
 				r = n->client->floating_rectangle;
+				if (center_pseudo_tiled) {
+					r.x = rect.x - bw + (rect.width - wg - r.width) / 2;
+					r.y = rect.y - bw + (rect.height - wg - r.height) / 2;
+				} else {
+					r.x = rect.x;
+					r.y = rect.y;
+				}
 			}
+		/* floating clients */
+		} else if (s == STATE_FLOATING) {
+			r = n->client->floating_rectangle;
+		/* fullscreen clients */
 		} else {
-			/* fullscreen clients */
 			r = m->rectangle;
 		}
 
@@ -231,8 +228,9 @@ void insert_node(monitor_t *m, desktop_t *d, node_t *n, node_t *f)
 						c->second_child = n;
 						rot = 270;
 					}
-					if (!n->client->floating)
+					if (IS_TILED(n->client)) {
 						rotate_tree(p, rot);
+					}
 					n->birth_rotation = rot;
 				}
 				break;
@@ -331,20 +329,23 @@ void focus_node(monitor_t *m, desktop_t *d, node_t *n)
 			n->client->urgent = false;
 			put_status(SBSC_MASK_REPORT);
 		}
-		if (d->focus != NULL && n != d->focus && d->focus->client->fullscreen && stack_cmp(n->client, d->focus->client) <= 0) {
-			set_fullscreen(d->focus, false);
-			arrange(m, d);
+		if (d->focus != NULL && n != d->focus && stack_cmp(n->client, d->focus->client) < 0) {
+			neutralize_obscuring_windows(m, d, n);
 		}
 	}
 
 	if (mon != m) {
-		for (desktop_t *cd = mon->desk_head; cd != NULL; cd = cd->next)
+		for (desktop_t *cd = mon->desk_head; cd != NULL; cd = cd->next) {
 			window_draw_border(cd->focus, true, false);
-		for (desktop_t *cd = m->desk_head; cd != NULL; cd = cd->next)
-			if (cd != d)
+		}
+		for (desktop_t *cd = m->desk_head; cd != NULL; cd = cd->next) {
+			if (cd != d) {
 				window_draw_border(cd->focus, true, true);
-		if (d->focus == n)
+			}
+		}
+		if (d->focus == n) {
 			window_draw_border(n, true, true);
+		}
 	}
 
 	if (d->focus != n) {
@@ -381,7 +382,7 @@ void focus_node(monitor_t *m, desktop_t *d, node_t *n)
 	}
 
 	if (pointer_follows_focus) {
-		center_pointer(get_rectangle(n->client));
+		center_pointer(get_rectangle(m, n->client));
 	}
 
 	ewmh_update_active_window();
@@ -410,11 +411,11 @@ client_t *make_client(xcb_window_t win, unsigned int border_width)
 {
 	client_t *c = malloc(sizeof(client_t));
 	c->window = win;
+	c->state = c->last_state = STATE_TILED;
 	c->layer = c->last_layer = LAYER_NORMAL;
 	snprintf(c->class_name, sizeof(c->class_name), "%s", MISSING_VALUE);
 	snprintf(c->instance_name, sizeof(c->instance_name), "%s", MISSING_VALUE);
 	c->border_width = border_width;
-	c->pseudo_tiled = c->floating = c->fullscreen = false;
 	c->locked = c->sticky = c->urgent = c->private = c->icccm_focus = false;
 	xcb_icccm_get_wm_protocols_reply_t protocols;
 	if (xcb_icccm_get_wm_protocols_reply(dpy, xcb_icccm_get_wm_protocols(dpy, win, ewmh->WM_PROTOCOLS), &protocols, NULL) == 1) {
@@ -479,7 +480,7 @@ void closest_public(desktop_t *d, node_t *n, node_t **closest, node_t **public)
 	while (prev != NULL || next != NULL) {
 #define TESTLOOP(n) \
 		if (n != NULL) { \
-			if (!n->client->floating) { \
+			if (IS_TILED(n->client)) { \
 				if (n->privacy_level == 0) { \
 					if (n->parent == NULL || n->parent->privacy_level == 0) { \
 						*public = n; \
@@ -544,7 +545,7 @@ node_t *prev_leaf(node_t *n, node_t *r)
 node_t *next_tiled_leaf(desktop_t *d, node_t *n, node_t *r)
 {
 	node_t *next = next_leaf(n, r);
-	if (next == NULL || !next->client->floating)
+	if (next == NULL || IS_TILED(next->client))
 		return next;
 	else
 		return next_tiled_leaf(d, next, r);
@@ -553,24 +554,11 @@ node_t *next_tiled_leaf(desktop_t *d, node_t *n, node_t *r)
 node_t *prev_tiled_leaf(desktop_t *d, node_t *n, node_t *r)
 {
 	node_t *prev = prev_leaf(n, r);
-	if (prev == NULL || !prev->client->floating)
+	if (prev == NULL || IS_TILED(prev->client))
 		return prev;
 	else
 		return prev_tiled_leaf(d, prev, r);
 }
-
-/* bool is_adjacent(node_t *a, node_t *r) */
-/* { */
-/*	   node_t *f = r->parent; */
-/*	   node_t *p = a; */
-/*	   bool first_child = is_first_child(r); */
-/*	   while (p != r) { */
-/*		   if (p->parent->split_type == f->split_type && is_first_child(p) == first_child) */
-/*			   return false; */
-/*		   p = p->parent; */
-/*	   } */
-/*	   return true; */
-/* } */
 
 /* Returns true if *b* is adjacent to *a* in the direction *dir* */
 bool is_adjacent(node_t *a, node_t *b, direction_t dir)
@@ -615,8 +603,8 @@ node_t *find_fence(node_t *n, direction_t dir)
 
 node_t *nearest_neighbor(monitor_t *m, desktop_t *d, node_t *n, direction_t dir, client_select_t sel)
 {
-	if (n == NULL || n->client->fullscreen ||
-	    (d->layout == LAYOUT_MONOCLE && !n->client->floating))
+	if (n == NULL || IS_FULLSCREEN(n->client) ||
+	    (d->layout == LAYOUT_MONOCLE && IS_TILED(n->client)))
 		return NULL;
 
 	node_t *nearest = NULL;
@@ -634,8 +622,9 @@ node_t *nearest_neighbor(monitor_t *m, desktop_t *d, node_t *n, direction_t dir,
 
 node_t *nearest_from_tree(monitor_t *m, desktop_t *d, node_t *n, direction_t dir, client_select_t sel)
 {
-    if (n == NULL)
+    if (n == NULL) {
         return NULL;
+    }
 
     node_t *fence = find_fence(n, dir);
 
@@ -644,24 +633,27 @@ node_t *nearest_from_tree(monitor_t *m, desktop_t *d, node_t *n, direction_t dir
 
     node_t *nearest = NULL;
 
-    if (dir == DIR_UP || dir == DIR_LEFT)
+    if (dir == DIR_UP || dir == DIR_LEFT) {
         nearest = second_extrema(fence->first_child);
-    else if (dir == DIR_DOWN || dir == DIR_RIGHT)
+    } else if (dir == DIR_DOWN || dir == DIR_RIGHT) {
         nearest = first_extrema(fence->second_child);
+    }
 
 	coordinates_t ref = {m, d, n};
 	coordinates_t loc = {m, d, nearest};
 
-	if (node_matches(&loc, &ref, sel))
+	if (node_matches(&loc, &ref, sel)) {
 		return nearest;
-	else
+	} else {
 		return NULL;
+	}
 }
 
 node_t *nearest_from_history(monitor_t *m, desktop_t *d, node_t *n, direction_t dir, client_select_t sel)
 {
-	if (n == NULL || n->client->floating)
+	if (n == NULL || !IS_TILED(n->client)) {
 		return NULL;
+	}
 
 	node_t *target = find_fence(n, dir);
 	if (target == NULL)
@@ -699,7 +691,7 @@ node_t *nearest_from_distance(monitor_t *m, desktop_t *d, node_t *n, direction_t
 
 	node_t *target = NULL;
 
-	if (!n->client->floating) {
+	if (IS_TILED(n->client)) {
 		target = find_fence(n, dir);
 		if (target == NULL)
 			return NULL;
@@ -724,8 +716,8 @@ node_t *nearest_from_distance(monitor_t *m, desktop_t *d, node_t *n, direction_t
 		coordinates_t loc = {m, d, a};
 		if (a == n ||
 		    !node_matches(&loc, &ref, sel) ||
-		    !a->client->floating != !n->client->floating ||
-		    (!a->client->floating && !is_adjacent(n, a, dir)))
+		    IS_TILED(a->client) != IS_TILED(n->client) ||
+		    (IS_TILED(a->client) && !is_adjacent(n, a, dir)))
 			continue;
 
 		get_side_handle(a->client, dir2, &pt2);
@@ -769,7 +761,7 @@ int tiled_count(desktop_t *d)
 {
 	int cnt = 0;
 	for (node_t *f = first_extrema(d->root); f != NULL; f = next_leaf(f, d->root)) {
-		if (!f->client->floating) {
+		if (IS_TILED(f->client)) {
 			cnt++;
 		}
 	}
@@ -787,7 +779,7 @@ node_t *find_biggest(monitor_t *m, desktop_t *d, node_t *n, client_select_t sel)
 
 	for (node_t *f = first_extrema(d->root); f != NULL; f = next_leaf(f, d->root)) {
 		coordinates_t loc = {m, d, f};
-		if (f->client->floating || !node_matches(&loc, &ref, sel))
+		if (IS_FLOATING(f->client) || !node_matches(&loc, &ref, sel))
 			continue;
 		int f_area = tiled_area(f);
 		if (r == NULL) {

Sourcedeps
@@ -1,18 +1,18 @@
 bspc.o: bspc.c common.h helpers.h
 bspwm.o: bspwm.c bspwm.h common.h desktop.h events.h ewmh.h helpers.h history.h messages.h monitor.h rule.h settings.h stack.h subscribe.h types.h window.h
-desktop.o: desktop.c bspwm.h desktop.h ewmh.h helpers.h history.h monitor.h query.h settings.h tree.h types.h window.h
+desktop.o: desktop.c bspwm.h desktop.h ewmh.h helpers.h history.h monitor.h query.h settings.h subscribe.h tree.h types.h window.h
 events.o: events.c bspwm.h events.h ewmh.h helpers.h monitor.h query.h settings.h tree.h types.h window.h
 ewmh.o: ewmh.c bspwm.h ewmh.h helpers.h settings.h tree.h types.h
 helpers.o: helpers.c bspwm.h helpers.h types.h
 history.o: history.c bspwm.h helpers.h query.h types.h
 messages.o: messages.c bspwm.h common.h desktop.h ewmh.h helpers.h history.h messages.h monitor.h pointer.h query.h restore.h rule.h settings.h subscribe.h tree.h types.h window.h
-monitor.o: monitor.c bspwm.h desktop.h ewmh.h helpers.h history.h monitor.h query.h settings.h tree.h types.h window.h
+monitor.o: monitor.c bspwm.h desktop.h ewmh.h helpers.h history.h monitor.h query.h settings.h subscribe.h tree.h types.h window.h
 pointer.o: pointer.c bspwm.h helpers.h monitor.h pointer.h query.h settings.h stack.h tree.h types.h window.h
-query.o: query.c bspwm.h desktop.h helpers.h history.h messages.h monitor.h query.h tree.h types.h
+query.o: query.c bspwm.h desktop.h helpers.h history.h messages.h monitor.h query.h subscribe.h tree.h types.h
 restore.o: restore.c bspwm.h desktop.h ewmh.h helpers.h history.h monitor.h query.h restore.h settings.h stack.h tree.h types.h
-rule.o: rule.c bspwm.h ewmh.h helpers.h messages.h rule.h settings.h types.h window.h
+rule.o: rule.c bspwm.h ewmh.h helpers.h messages.h rule.h settings.h subscribe.h types.h window.h
 settings.o: settings.c bspwm.h helpers.h settings.h types.h
 stack.o: stack.c bspwm.h helpers.h stack.h types.h window.h
 subscribe.o: subscribe.c bspwm.h helpers.h settings.h subscribe.h tree.h types.h
-tree.o: tree.c bspwm.h desktop.h ewmh.h helpers.h history.h monitor.h query.h settings.h stack.h tree.h types.h window.h
-window.o: window.c bspwm.h ewmh.h helpers.h messages.h monitor.h query.h rule.h settings.h stack.h tree.h types.h window.h
+tree.o: tree.c bspwm.h desktop.h ewmh.h helpers.h history.h monitor.h query.h settings.h stack.h subscribe.h tree.h types.h window.h
+window.o: window.c bspwm.h ewmh.h helpers.h messages.h monitor.h query.h rule.h settings.h stack.h subscribe.h tree.h types.h window.h

desktop.c
@@ -76,8 +76,9 @@ void change_layout(monitor_t *m, desktop_t *d, layout_t l)
 	put_status(SBSC_MASK_DESKTOP_LAYOUT, "desktop_layout %s %s %s\n", m->name, d->name, l==LAYOUT_TILED?"tiled":"monocle");
 	d->layout = l;
 	arrange(m, d);
-	if (d == m->desk)
+	if (d == m->desk) {
 		put_status(SBSC_MASK_REPORT);
+	}
 }
 
 void transfer_desktop(monitor_t *ms, monitor_t *md, desktop_t *d)
@@ -92,19 +93,23 @@ void transfer_desktop(monitor_t *ms, monitor_t *md, desktop_t *d)
 	insert_desktop(md, d);
 
 	if (d == dd) {
-		if (ms->desk != NULL)
+		if (ms->desk != NULL) {
 			show_desktop(ms->desk);
-		if (md->desk != d)
+		}
+		if (md->desk != d) {
 			hide_desktop(d);
+		}
 	}
 
-	for (node_t *n = first_extrema(d->root); n != NULL; n = next_leaf(n, d->root))
+	for (node_t *n = first_extrema(d->root); n != NULL; n = next_leaf(n, d->root)) {
 		translate_client(ms, md, n->client);
+	}
 
 	arrange(md, d);
 
-	if (d != dd && md->desk == d)
+	if (d != dd && md->desk == d) {
 		show_desktop(d);
+	}
 
 	history_transfer_desktop(md, d);
 
@@ -139,7 +144,6 @@ void initialize_desktop(desktop_t *d)
 	d->top_padding = d->right_padding = d->bottom_padding = d->left_padding = 0;
 	d->window_gap = window_gap;
 	d->border_width = border_width;
-	d->floating = false;
 }
 
 void insert_desktop(monitor_t *m, desktop_t *d)

doc/bspwm.1
@@ -2,12 +2,12 @@
 .\"     Title: bspwm
 .\"    Author: [see the "Author" section]
 .\" Generator: DocBook XSL Stylesheets v1.78.1 <http://docbook.sf.net/>
-.\"      Date: 10/27/2015
+.\"      Date: 11/05/2015
 .\"    Manual: Bspwm Manual
 .\"    Source: Bspwm 0.9
 .\"  Language: English
 .\"
-.TH "BSPWM" "1" "10/27/2015" "Bspwm 0\&.9" "Bspwm Manual"
+.TH "BSPWM" "1" "11/05/2015" "Bspwm 0\&.9" "Bspwm Manual"
 .\" -----------------------------------------------------------------
 .\" * Define some portability stuff
 .\" -----------------------------------------------------------------
@@ -167,7 +167,7 @@ Select a window\&.
 .\}
 .nf
 WINDOW_SEL := <window_id>
-            | (DIR|CYCLE_DIR|biggest|last|focused|older|newer)[\&.floating|\&.tiled][\&.like|\&.unlike][\&.manual|\&.automatic][\&.urgent|\&.nonurgent][\&.local|\&.foreign][\&.focused|\&.unfocused][\&.below|\&.normal|\&.above][\&.fullscreen|\&.nonfullscreen][\&.sticky|\&.nonsticky][\&.public|\&.private][\&.locked|\&.unlocked][\&.pseudotiled|\&.nonpseudotiled]
+            | (DIR|CYCLE_DIR|biggest|last|focused|older|newer)[\&.manual|\&.automatic][\&.tiled|nontiled][\&.pseudotiled|\&.nonpseudotiled][\&.floating|\&.nonfloating][\&.fullscreen|\&.nonfullscreen][\&.below|\&.normal|\&.above][\&.local|\&.foreign][\&.like|\&.unlike][\&.focused|\&.unfocused][\&.urgent|\&.nonurgent][\&.sticky|\&.nonsticky][\&.public|\&.private][\&.locked|\&.unlocked]
 .fi
 .if n \{\
 .RE
@@ -225,24 +225,44 @@ Selects the window newer than the focused window in the history\&.
 \fBModifiers\fR
 .RS 4
 .PP
-floating
-.RS 4
-Only consider floating windows\&.
-.RE
-.PP
 tiled
 .RS 4
 Only consider tiled windows\&.
 .RE
 .PP
-like
+nontiled
 .RS 4
-Only consider windows that have the same class as the current window\&.
+Only consider tiled windows\&.
 .RE
 .PP
-unlike
+pseudotiled
 .RS 4
-Only consider windows that have a different class than the current window\&.
+Only consider pseudo\-tiled windows\&.
+.RE
+.PP
+nonpseudotiled
+.RS 4
+Only consider non pseudo\-tiled windows\&.
+.RE
+.PP
+floating
+.RS 4
+Only consider floating windows\&.
+.RE
+.PP
+nonfloating
+.RS 4
+Only consider non floating windows\&.
+.RE
+.PP
+fullscreen
+.RS 4
+Only consider fullscreen windows\&.
+.RE
+.PP
+nonfullscreen
+.RS 4
+Only consider non fullscreen windows\&.
 .RE
 .PP
 manual
@@ -253,6 +273,41 @@ Only consider windows in manual splitting mode\&.
 automatic
 .RS 4
 Only consider windows in automatic splitting mode\&.
+.RE
+.PP
+focused
+.RS 4
+Only consider focused windows\&.
+.RE
+.PP
+unfocused
+.RS 4
+Only consider unfocused windows\&.
+.RE
+.PP
+below
+.RS 4
+Only consider windows of the BELOW layer\&.
+.RE
+.PP
+normal
+.RS 4
+Only consider windows of the NORMAL layer\&.
+.RE
+.PP
+above
+.RS 4
+Only consider windows of the ABOVE layer\&.
+.RE
+.PP
+like
+.RS 4
+Only consider windows that have the same class as the current window\&.
+.RE
+.PP
+unlike
+.RS 4
+Only consider windows that have a different class than the current window\&.
 .RE
 .PP
 local
@@ -285,16 +340,6 @@ nonurgent
 Only consider non urgent windows\&.
 .RE
 .PP
-pseudotiled
-.RS 4
-Only consider pseudo\-tiled windows\&.
-.RE
-.PP
-nonpseudotiled
-.RS 4
-Only consider non pseudo\-tiled windows\&.
-.RE
-.PP
 sticky
 .RS 4
 Only consider sticky windows\&.
@@ -313,46 +358,6 @@ Only consider locked windows\&.
 unlocked
 .RS 4
 Only consider non locked windows\&.
-.RE
-.PP
-nonfullscreen
-.RS 4
-Only consider non fullscreen windows\&.
-.RE
-.PP
-fullscreen
-.RS 4
-Only consider fullscreen windows\&.
-.RE
-.PP
-nonfullscreen
-.RS 4
-Only consider non fullscreen windows\&.
-.RE
-.PP
-focused
-.RS 4
-Only consider focused windows\&.
-.RE
-.PP
-unfocused
-.RS 4
-Only consider unfocused windows\&.
-.RE
-.PP
-below
-.RS 4
-Only consider windows of the BELOW layer\&.
-.RE
-.PP
-normal
-.RS 4
-Only consider windows of the NORMAL layer\&.
-.RE
-.PP
-above
-.RS 4
-Only consider windows of the ABOVE layer\&.
 .RE
 .RE
 .SS "Desktop"
@@ -543,9 +548,9 @@ Only consider monitors where the focused desktop is free\&.
 .RE
 .SH "WINDOW STATES"
 .PP
-floating
+tiled
 .RS 4
-Can be moved/resized freely\&. Although it doesn\(cqt occupy any tiling space, it is still part of the window tree\&.
+Its size and position are determined by the splitting type and ratio of each node of its path in the window tree\&.
 .RE
 .PP
 pseudo_tiled
@@ -553,10 +558,16 @@ pseudo_tiled
 Has an unrestricted size while being centered in its tiling space\&.
 .RE
 .PP
+floating
+.RS 4
+Can be moved/resized freely\&. Although it doesn\(cqt occupy any tiling space, it is still part of the window tree\&.
+.RE
+.PP
 fullscreen
 .RS 4
-Fills its monitor rectangle and has no borders\&.
+Fills its monitor rectangle and has no borders\&. It is send in the ABOVE layer by default\&.
 .RE
+.SH "WINDOW FLAGS"
 .PP
 locked
 .RS 4
@@ -577,13 +588,13 @@ Tries to keep the same tiling position/size\&.
 .PP
 urgent
 .RS 4
-Has its urgency hint set\&.
+Has its urgency hint set\&. This flag is set externally\&.
 .RE
 .SH "STACKING LAYERS"
 .sp
 There\(cqs three stacking layers: BELOW, NORMAL and ABOVE\&.
 .sp
-In each layer, floating windows are stacked above tiled windows\&.
+In each layer, the window are orderered as follow: tiled & pseudo\-tiled < fullscreen < floating\&.
 .SH "COMMANDS"
 .SS "Window"
 .sp
@@ -653,9 +664,14 @@ Set or change the splitting ratio of the edge located in the given direction in 
 Rotate the tree holding the edge located in the given direction in relation to the selected window\&.
 .RE
 .PP
-\fB\-t\fR, \fB\-\-toggle\fR floating|fullscreen|pseudo_tiled|locked|sticky|private[=on|off]
+\fB\-t\fR, \fB\-\-state\fR tiled|pseudo_tiled|floating|fullscreen
 .RS 4
-Set or toggle the given state for the selected window\&.
+Set the state of the selected window\&.
+.RE
+.PP
+\fB\-g\fR, \fB\-\-flag\fR locked|sticky|private[=on|off]
+.RS 4
+Set or toggle the given flag for the selected window\&.
 .RE
 .PP
 \fB\-l\fR, \fB\-\-layer\fR below|normal|above
@@ -757,11 +773,6 @@ Adjust the split ratios of the tree of the selected desktop so that all windows 
 \fB\-C\fR, \fB\-\-circulate\fR forward|backward
 .RS 4
 Circulate the leaves of the tree of the selected desktop\&.
-.RE
-.PP
-\fB\-t\fR, \fB\-\-toggle\fR floating[=on|off]
-.RS 4
-Set or toggle the given state for the selected desktop\&.
 .RE
 .RE
 .SS "Monitor"
@@ -1015,7 +1026,7 @@ rule \fIOPTIONS\fR
 \fBOptions\fR
 .RS 4
 .PP
-\fB\-a\fR, \fB\-\-add\fR <class_name>|<instance_name>|* [\fB\-o\fR|\fB\-\-one\-shot\fR] [monitor=MONITOR_SEL|desktop=DESKTOP_SEL|window=WINDOW_SEL] [(floating|fullscreen|pseudo_tiled|locked|sticky|private|center|follow|manage|focus|border)=(on|off)] [layer=LAYER] [split_dir=DIR] [split_ratio=RATIO]
+\fB\-a\fR, \fB\-\-add\fR <class_name>|<instance_name>|* [\fB\-o\fR|\fB\-\-one\-shot\fR] [monitor=MONITOR_SEL|desktop=DESKTOP_SEL|window=WINDOW_SEL] [state=STATE] [layer=LAYER] [split_dir=DIR] [split_ratio=RATIO] [(locked|sticky|private|center|follow|manage|focus|border)=(on|off)]
 .RS 4
 Create a new rule\&.
 .RE

events.c
@@ -93,7 +93,7 @@ void configure_request(xcb_generic_event_t *evt)
 	client_t *c = (is_managed ? loc.node->client : NULL);
 	int w = 0, h = 0;
 
-	if (is_managed && !c->floating) {
+	if (is_managed && !IS_FLOATING(c)) {
 		if (e->value_mask & XCB_CONFIG_WINDOW_X)
 			c->floating_rectangle.x = e->x;
 		if (e->value_mask & XCB_CONFIG_WINDOW_Y)
@@ -114,14 +114,10 @@ void configure_request(xcb_generic_event_t *evt)
 		}
 
 		xcb_configure_notify_event_t evt;
-		xcb_rectangle_t rect;
 		xcb_window_t win = c->window;
 		unsigned int bw = c->border_width;
 
-		if (c->fullscreen)
-			rect = loc.monitor->rectangle;
-		else
-			rect = c->tiled_rectangle;
+		xcb_rectangle_t rect = get_rectangle(loc.monitor, c);
 
 		evt.response_type = XCB_CONFIGURE_NOTIFY;
 		evt.event = win;
@@ -136,8 +132,9 @@ void configure_request(xcb_generic_event_t *evt)
 
 		xcb_send_event(dpy, false, win, XCB_EVENT_MASK_STRUCTURE_NOTIFY, (const char *) &evt);
 
-		if (c->pseudo_tiled)
+		if (c->state == STATE_PSEUDO_TILED) {
 			arrange(loc.monitor, loc.desktop);
+		}
 	} else {
 		uint16_t mask = 0;
 		uint32_t values[7];
@@ -195,8 +192,9 @@ void configure_request(xcb_generic_event_t *evt)
 		xcb_configure_window(dpy, e->window, mask, values);
 	}
 
-	if (is_managed)
+	if (is_managed) {
 		translate_client(monitor_from_client(c), loc.monitor, c);
+	}
 }
 
 void destroy_notify(xcb_generic_event_t *evt)
@@ -382,41 +380,46 @@ void motion_notify(xcb_generic_event_t *evt)
 void handle_state(monitor_t *m, desktop_t *d, node_t *n, xcb_atom_t state, unsigned int action)
 {
 	if (state == ewmh->_NET_WM_STATE_FULLSCREEN) {
-		if (action == XCB_EWMH_WM_STATE_ADD)
-			set_fullscreen(n, true);
-		else if (action == XCB_EWMH_WM_STATE_REMOVE)
-			set_fullscreen(n, false);
-		else if (action == XCB_EWMH_WM_STATE_TOGGLE)
-			set_fullscreen(n, !n->client->fullscreen);
+		if (action == XCB_EWMH_WM_STATE_ADD) {
+			set_state(m, d, n, STATE_FULLSCREEN);
+		} else if (action == XCB_EWMH_WM_STATE_REMOVE) {
+			set_state(m, d, n, n->client->last_state);
+		} else if (action == XCB_EWMH_WM_STATE_TOGGLE) {
+			set_state(m, d, n, IS_FULLSCREEN(n->client) ? n->client->last_state : STATE_FULLSCREEN);
+		}
 		arrange(m, d);
 	} else if (state == ewmh->_NET_WM_STATE_BELOW) {
-		if (action == XCB_EWMH_WM_STATE_ADD)
-			set_layer(n, LAYER_BELOW);
-		else if (action == XCB_EWMH_WM_STATE_REMOVE)
-			set_layer(n, LAYER_NORMAL);
-		else if (action == XCB_EWMH_WM_STATE_TOGGLE)
-			set_layer(n, n->client->layer == LAYER_BELOW ? LAYER_NORMAL : LAYER_BELOW);
+		if (action == XCB_EWMH_WM_STATE_ADD) {
+			set_layer(m, d, n, LAYER_BELOW);
+		} else if (action == XCB_EWMH_WM_STATE_REMOVE) {
+			set_layer(m, d, n, LAYER_NORMAL);
+		} else if (action == XCB_EWMH_WM_STATE_TOGGLE) {
+			set_layer(m, d, n, n->client->layer == LAYER_BELOW ? n->client->last_layer : LAYER_BELOW);
+		}
 	} else if (state == ewmh->_NET_WM_STATE_ABOVE) {
-		if (action == XCB_EWMH_WM_STATE_ADD)
-			set_layer(n, LAYER_ABOVE);
-		else if (action == XCB_EWMH_WM_STATE_REMOVE)
-			set_layer(n, LAYER_NORMAL);
-		else if (action == XCB_EWMH_WM_STATE_TOGGLE)
-			set_layer(n, n->client->layer == LAYER_ABOVE ? LAYER_NORMAL : LAYER_ABOVE);
+		if (action == XCB_EWMH_WM_STATE_ADD) {
+			set_layer(m, d, n, LAYER_ABOVE);
+		} else if (action == XCB_EWMH_WM_STATE_REMOVE) {
+			set_layer(m, d, n, n->client->last_layer);
+		} else if (action == XCB_EWMH_WM_STATE_TOGGLE) {
+			set_layer(m, d, n, n->client->layer == LAYER_ABOVE ? n->client->last_layer : LAYER_ABOVE);
+		}
 	} else if (state == ewmh->_NET_WM_STATE_STICKY) {
-		if (action == XCB_EWMH_WM_STATE_ADD)
+		if (action == XCB_EWMH_WM_STATE_ADD) {
 			set_sticky(m, d, n, true);
-		else if (action == XCB_EWMH_WM_STATE_REMOVE)
+		} else if (action == XCB_EWMH_WM_STATE_REMOVE) {
 			set_sticky(m, d, n, false);
-		else if (action == XCB_EWMH_WM_STATE_TOGGLE)
+		} else if (action == XCB_EWMH_WM_STATE_TOGGLE) {
 			set_sticky(m, d, n, !n->client->sticky);
+		}
 	} else if (state == ewmh->_NET_WM_STATE_DEMANDS_ATTENTION) {
-		if (action == XCB_EWMH_WM_STATE_ADD)
+		if (action == XCB_EWMH_WM_STATE_ADD) {
 			set_urgency(m, d, n, true);
-		else if (action == XCB_EWMH_WM_STATE_REMOVE)
+		} else if (action == XCB_EWMH_WM_STATE_REMOVE) {
 			set_urgency(m, d, n, false);
-		else if (action == XCB_EWMH_WM_STATE_TOGGLE)
+		} else if (action == XCB_EWMH_WM_STATE_TOGGLE) {
 			set_urgency(m, d, n, !n->client->urgent);
+		}
 	}
 }
 

examples/bspwmrc
@@ -10,8 +10,8 @@ bspc config focus_by_distance   true
 
 bspc monitor -d I II III IV V VI VII VIII IX X
 
-bspc rule -a Gimp desktop=^8 follow=on floating=on
+bspc rule -a Gimp desktop=^8 state=floating follow=on
 bspc rule -a Chromium desktop=^2
-bspc rule -a mplayer2 floating=on
+bspc rule -a mplayer2 state=floating
 bspc rule -a Kupfer.py focus=on
 bspc rule -a Screenkey manage=off

examples/sxhkdrc
@@ -8,14 +8,11 @@ super + alt + Escape
 super + w
 	bspc window -c
 
-super + t
+super + n
 	bspc desktop -l next
 
-super + b
-	bspc desktop -B
-
-super + {s,f}
-	bspc window -t {floating,fullscreen}
+super + {t,p,s,f}
+	bspc window -t {tiled,pseudo_tiled,floating,fullscreen}
 
 super + {grave,Tab}
 	bspc {window,desktop} -f last
@@ -61,8 +58,11 @@ super + alt + shift + {h,j,k,l}
 super + ctrl + {1-9}
 	bspc window -r 0.{1-9}
 
+super + {Left,Down,Up,Right}
+	xdo move {-x -20,-y +20,-y -20,-x +20}
+
 super + {_,shift + }{1-9,0}
-	bspc {desktop -f,window -d} ^{1-9,10}
+	bspc {desktop -f,window -d} '^{1-9,10}'
 
 ~button1
 	bspc pointer -g focus

helpers.h
@@ -37,6 +37,10 @@
 #define BOOLSTR(A)        ((A) ? "true" : "false")
 #define ONOFFSTR(A)       ((A) ? "on" : "off")
 #define LAYERSTR(A)       ((A) == LAYER_BELOW ? "below" : ((A) == LAYER_NORMAL ? "normal" : "above"))
+#define STATESTR(A)       ((A) == STATE_TILED ? "tiled" : ((A) == STATE_FLOATING ? "floating" : ((A) == STATE_FULLSCREEN ? "fullscreen" : "pseudo_tiled")))
+#define IS_TILED(c)       (c->state == STATE_TILED || c->state == STATE_PSEUDO_TILED)
+#define IS_FLOATING(c)    (c->state == STATE_FLOATING)
+#define IS_FULLSCREEN(c)  (c->state == STATE_FULLSCREEN)
 
 #define XCB_CONFIG_WINDOW_X_Y               XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y
 #define XCB_CONFIG_WINDOW_WIDTH_HEIGHT      XCB_CONFIG_WINDOW_WIDTH | XCB_CONFIG_WINDOW_HEIGHT

commit 8aa8f9e408229e7f13a8f2aedefafa7031831e45

Fix regression introduced by f28f334
(Set `tiled_rectangle` in `apply_layout`.)

tree.c
@@ -97,6 +97,7 @@ void apply_layout(monitor_t *m, desktop_t *d, node_t *n, xcb_rectangle_t rect, x
 					r.y = rect.y;
 				}
 			}
+			n->client->tiled_rectangle = r;
 		/* floating clients */
 		} else if (s == STATE_FLOATING) {
 			r = n->client->floating_rectangle;